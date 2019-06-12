80be32d152bd001a9253bbf487b13c6559ca3d76
chenming9 <yamateh_ibm@163.com>
2019-06-12 10:10:49 +0800

    feat: change base url for production.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 // vue.config.js
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/website/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   css: {
     loaderOptions: {
       sass: {
